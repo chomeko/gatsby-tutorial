78afece58fcccbe0e2e030cd8bd76c2ab5325f2d
chomeko3o <kusyopi@gmail.com>
2020-02-16 17:02:02 +0900

    Layoutの追加方法

---
+++ src/components/layout.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./layout.css"
+
+export default ({ children }) => <div>{children}</div>
+
+//src/components/layout.css
+//src/components/layout.js
+//作成してsrc/pages/index.jsにimport Layout from "../components/layout"の追加で反映
