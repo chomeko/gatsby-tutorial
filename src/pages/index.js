ba4216a2d05d5d253869a1e4120ce71429dfc931
chomeko3o <kusyopi@gmail.com>
2020-02-16 19:06:41 +0900

    その他修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
-import styles from "../styles/index.module.css"
+import styles from "../pages/index.module.css"
 import Layout from "../components/layout"
+import "../styles/index.css"
 
 const User = props => (
   <div className={styles.user}>
@@ -17,18 +18,20 @@ export default () => (
     <section className="index">
       <p>INDEXページ</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-      <h1>About CSS Modules</h1>
-      <p>CSS Modules are cool</p>
-      <User
-        username="Jane Doe"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
-      <User
-        username="Bob Smith"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
+      <div className="wrapper">
+        <h1>About CSS Modules</h1>
+        <p>CSS Modules are cool</p>
+        <User
+          username="Jane Doe"
+          avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+          excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+        <User
+          username="Bob Smith"
+          avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+          excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+      </div>
     </section>
   </Layout>
 )
